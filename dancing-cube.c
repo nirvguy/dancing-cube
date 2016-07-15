11082afc83e23ce0351f7db3420eb187bca9a362
nirvguy <nirvguy@gmail.com>
2016-07-15 16:01:19 -0300

    Basic setup of the window

---
+++ dancing-cube.c
@@ -0,0 +1,72 @@
+/**
+ * dancing-cube, an OpenGL program for animate an array of cubes.
+ * Copyright (C) 2016 Juan Cruz Sosa (nirvguy@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+const GLfloat light_ambient[]  = {0.5  , 0.5   , 0.5 , 0.0};   // RGBA color of the ambient light
+const GLfloat light_diffuse[]  = {0.01 , 0.689 , 0.8 , 1.0};   // RGBA color of the diffuse light
+const GLfloat light_position[] = {1.0  , 1.0   , 1.0 , 0.0};   // Position of the light
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+
+void init()
+{
+	// Set background color to gray
+	glClearColor(0.2f, 0.2f, 0.2f, 0.2f);
+
+	// Set-up lights
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+
+	// Set-up perspective
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(45.0,    // Field of view angle in degrees
+		           1.0,     // Aspect ratio
+		           0.5,     // Z near
+		           20.0);   // Z far
+
+	glMatrixMode(GL_MODELVIEW);
+	gluLookAt(5.0, 5.0, 5.0,  // Eye position
+	          0.0, 0.0, 0.0,  // Center
+	          0.0, 0.0, 1.0); // Up vector
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutCreateWindow("dancing-cube");
+
+	glutDisplayFunc(display);
+
+	init();
+	glutMainLoop();
+
+	return 0;
+}
